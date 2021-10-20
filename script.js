b20c69b36b6f1b1f6cbba06d8c68b418c2719037
Isah Ibrahim <isahibn08@gmail.com>
2021-10-20 12:29:36 +0100

    created header and profile card

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("This is a test")
+const profileDropdown = document.querySelector('.drop-down');
+let dropIndicator = document.querySelector('#drop-indicator')
+
+profileDropdown.addEventListener('click' , myFunction)
+    
+function myFunction(){
+    let dropdownContent = document.querySelector('.dropdown-content');
+    if (dropdownContent.style.display === "none") {
+        dropIndicator.classList.remove('fa-chevron-down')
+        dropIndicator.classList.add('fa-chevron-up')
+        dropdownContent.style.display = "block";
+    } else {
+        dropIndicator.classList.add('fa-chevron-down')
+        dropIndicator.classList.remove('fa-chevron-up')
+        dropdownContent.style.display = "none";
+    }
+}
